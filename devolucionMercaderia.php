9ad391623294756f650a7bf01c3d9f6cb675904a
facundoaires <facuaires@gmail.com>
2023-07-09 20:36:58 -0300

    entrega1

--- devolucionMercaderia.php
+++ devolucionMercaderia.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pago Mercaderia<</title>
+    <title>Pago Mercaderia</title>
     <link rel="stylesheet" href="css/remito.css">
 </head>
 <body>
